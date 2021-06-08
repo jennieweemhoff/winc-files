ba858d2f6e0dc9116ce592d8ac262a1f3e89e42f
jennieweemhoff <jennie96@gmail.com>
2021-06-08 15:29:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,26 +2,25 @@ __winc_id__ = 'ae539110d03e49ea8738fd413ac44ba8'
 __human_name__ = 'files'
 
 import os 
-import glob
 import shutil
-import io
 
+cache = './cache'
+zip = 'data.zip'
 
 def clean_cache(): 
-    if os.path.exists('c:/Users/Jennie/files/cache'): 
-        cache = 'c:/Users/Jennie/files/cache'
+    if os.path.exists(cache): 
         for file in os.scandir(cache): 
             os.remove(file.path)
     else: 
-        os.makedirs('C:/Users/Jennie/files/cache')
+        os.makedirs(cache)
 
 def cache_zip(zip_file, cache_dir): 
     shutil.unpack_archive(zip_file, cache_dir)
 
 def cached_files(): 
     files_list = []
-    for entry in os.scandir(r'C:\Users\Jennie\files\cache'): 
-        files_list.append(entry.path)
+    for entry in os.scandir(cache): 
+        files_list.append(os.path.abspath(entry))
     return files_list
 
 def find_password(files_list):
@@ -30,16 +29,15 @@ def find_password(files_list):
             file = file.read()
             if 'password' in file: 
                 file_lines = file.split('\n')
-                password_line = file_lines[5]
-                password = password_line.split(' ')[1]
-                return password 
-                
-
-#zip = 'C:/Users/Jennie/files/data.zip'
-#cache = 'C:/Users/Jennie/files/cache'
+                for file_line in file_lines:
+                    if "password: " in file_line:
+                        password = file_line.split(' ')[1]
+                return password
 
 if __name__ == '__main__':
-    #cache_zip(zip, cache)
-    #clean_cache()
+    clean_cache()
+    cache_zip(zip, cache)
     files = cached_files()
     find_password(files)
+
+
